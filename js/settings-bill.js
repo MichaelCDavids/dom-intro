2478cdb56f49ed43bfcc347426591bbde1a23ad2
michael <michaelcdavids@gmail.com>
2018-04-26 09:30:52 +0200

    fixed bug on last widget: Cant add if critical level reached

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -18,26 +18,26 @@ document.addEventListener('DOMContentLoaded', function(){
   var settingsObject = SettingsFunctions();
 
   function addToBill(){
-
-    var checkedRadioBtnTwo = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-    if (checkedRadioBtnTwo){
-      var ItemType = checkedRadioBtnTwo.value;
-      settingsObject.calculateSettingsBill(ItemType);
-      radioSettingsCallTotal.innerHTML = settingsObject.allCalls().toFixed(2);
-      radioSettingsSmsTotal.innerHTML = settingsObject.allSms().toFixed(2);
-      radioSettingsAllTotal.innerHTML = settingsObject.allTotal().toFixed(2);
+    if(settingsObject.allTotal().toFixed(2)<settingsObject.CriticalValue()){
+      var checkedRadioBtnTwo = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+      if (checkedRadioBtnTwo){
+        var ItemType = checkedRadioBtnTwo.value;
+        settingsObject.calculateSettingsBill(ItemType);
+        radioSettingsCallTotal.innerHTML = settingsObject.allCalls().toFixed(2);
+        radioSettingsSmsTotal.innerHTML = settingsObject.allSms().toFixed(2);
+        radioSettingsAllTotal.innerHTML = settingsObject.allTotal().toFixed(2);
+      }
+      if(settingsObject.allTotal()<settingsObject.WarningValue()){
+        radioSettingsAllTotal.classList.remove("danger");
+        radioSettingsAllTotal.classList.remove("warning");
+      }else if(settingsObject.allTotal()>settingsObject.WarningValue() && settingsObject.allTotal()<settingsObject.CriticalValue()){
+        radioSettingsAllTotal.classList.remove("danger");
+        radioSettingsAllTotal.classList.add("warning");
+      }else if(settingsObject.allTotal()>=settingsObject.CriticalValue()){
+        radioSettingsAllTotal.classList.remove("warning");
+        radioSettingsAllTotal.classList.add("danger");
+      }
     }
-    if(settingsObject.allTotal()<settingsObject.WarningValue()){
-      radioSettingsAllTotal.classList.remove("danger");
-      radioSettingsAllTotal.classList.remove("warning");
-    }else if(settingsObject.allTotal()>settingsObject.WarningValue() && settingsObject.allTotal()<settingsObject.CriticalValue()){
-      radioSettingsAllTotal.classList.remove("danger");
-      radioSettingsAllTotal.classList.add("warning");
-    }else if(settingsObject.allTotal()>=settingsObject.CriticalValue()){
-      radioSettingsAllTotal.classList.remove("warning");
-      radioSettingsAllTotal.classList.add("danger");
-    }
-
   }
 
   function valueUpdater(){
